dbf6f6b2ecee93451d26886f5f7811c79526e6cf
Sarah Allen <sarahallen@google.com>
2019-01-18 05:49:19 -0800

    completed koans and comments

--- about_basics.go
+++ about_basics.go
@@ -1,34 +1,39 @@
 package go_koans
 
 func aboutBasics() {
-	assert(__bool__ == true)  // what is truth?
-	assert(__bool__ != false) // in it there is nothing false
+	assert(true == true)  // what is truth?
+	assert(true != false) // in it there is nothing false
 
-	var i int = __int__
+	var i int = 1.0
 	assert(i == 1.0000000000000000000000000000000000000) // precision is in the eye of the beholder
 
-	k := __int__ //short assignment can be used, as well
+	k := 1 //short assignment can be used, as well
 	assert(k == 1.0000000000000000000000000000000000000)
 
-	assert(5%2 == __int__)
-	assert(5*2 == __int__)
-	assert(5^2 == __int__)
+	assert(5%2 == 1)
+	assert(5*2 == 10)
+	assert(5/2 == 2)
+	assert(5^2 == 7)
 
 	var x int
-	assert(x == __int__) // zero values are valued in Go
+	assert(x == 0) // listen to the darkness of an unset variable
+	// listen to the darkness of an unset variable
+	// what is the value of a variable declaration?
+  // listen to the darkness of an unset variable
+	// what is the code that is not written?
 
 	var f float32
-	assert(f == __float32__) // for types of all types
+	assert(f == 0) // what is the code that is not written?
 
 	var s string
-	assert(s == __string__) // both typical or atypical types
+	assert(s == "") // consider the emptiness of a string
 
 	var c struct {
 		x int
 		f float32
 		s string
 	}
-	assert(c.x == __int__)     // and types within composite types
-	assert(c.f == __float32__) // which match the other types
-	assert(c.s == __string__)  // in a typical way
+	assert(c.x == 0)     // what is structured emptiness?
+	assert(c.f == 0) // what is the value of floating emptiness?
+	assert(c.s == "")  	// listen to the structure of a string
 }
--- about_strings.go
+++ about_strings.go
@@ -3,28 +3,35 @@ package go_koans
 import "fmt"
 
 func aboutStrings() {
-	assert("a"+__string__ == "abc") // string concatenation need not be difficult
-	assert(len("abc") == __int__)   // and bounds are thoroughly checked
+	assert("a"+"bc" == "abc") // concatenation is an operation on a string type
+	assert(len("abc") == 3)   // the len built-in function returns the length of v, according to its type
 
-	assert("abc"[0] == __byte__) // their contents are reminiscent of C
+	assert("走" == "走") // walk past the land of 256 characters
+	assert(len("走") == 3) // what is the length of a string?
 
-	assert("smith"[2:] == __string__)  // slicing may omit the end point
-	assert("smith"[:4] == __string__)  // or the beginning
-	assert("smith"[2:4] == __string__) // or neither
-	assert("smith"[:] == __string__)   // or both
+	assert("abc"[0] == 97) // reflect on the ascii table, which can encode simple scripts like English
+	assert("走"[1] == 181) // the expressiveness of human written language cannot be encoded in a byte
 
-	assert("smith" == __string__) // they can be compared directly
-	assert("smith" < __string__)  // i suppose maybe this could be useful.. someday
+	assert("smith"[2:] == "ith")   // slicing may omit the end point
+	assert("smith"[:4] == "smit")  // or the beginning
+	assert("smith"[2:4] == "it")   // or neither
+	assert("smith"[:] == "smith")  // or both
 
+	var s = "smith"
+	//assert.Equal(s, __string__, "The two words should be the same.")
+	assert(s ==  "smith") // they can be compared directly
+	assert("smith" <  "zzz")  // what is the order of a string?
+
+	// TODO: weird to introduce this before [] syntax
 	bytes := []byte{'a', 'b', 'c'}
-	assert(string(bytes) == __string__) // strings can be created from byte-slices
+	assert(string(bytes) == "abc") // strings can be created from byte-slices
 
 	bytes[0] = 'z'
-	assert(string(bytes) == __string__) // byte-slices can be mutated, although strings cannot
+	assert(string(bytes) == "zbc") // byte-slices can be mutated, although strings cannot
 
-	assert(fmt.Sprintf("hello %s", __string__) == "hello world") // our old friend sprintf returns
-	assert(fmt.Sprintf("hello \"%s\"", "world") == __string__)   // quoting is familiar
-	assert(fmt.Sprintf("hello %q", "world") == __string__)       // although it can be done more easily
+	assert(fmt.Sprintf("hello %s", "world") == "hello world") // our old friend sprintf returns
+	assert(fmt.Sprintf("hello \"%s\"", "world") == `hello "world"`)   // quoting is familiar
+	assert(fmt.Sprintf("hello %q", "world") == `hello "world"`)       // although it can be done more easily
 
-	assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == __string__) // "the root of all evil" is actually a misquotation, by the way
+	assert(fmt.Sprintf("your balance: %d and %0.2f", 3, 4.5589) == "your balance: 3 and 4.56") // "the root of all evil" is actually a misquotation, by the way
 }
